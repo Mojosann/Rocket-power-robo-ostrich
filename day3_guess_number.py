30a68510c34017d3a95a2616afc9d463d8f2b4cf
Mojosann <74448163+Mojosann@users.noreply.github.com>
2021-03-28 02:57:18 +0800

    Guess number
    input random numbers to predict the computer's answer.

---
+++ day3_guess_number.py
@@ -0,0 +1,20 @@
+# guess_basic version
+# 產生一個隨機整數1~100(不用印出來)
+# 讓使用者重複輸入數字去猜 > 迴圈
+# 猜對的話 印出"終於猜對了!"
+# 猜錯的話 要告訴他比答案大/小
+
+import random
+
+r = random.randint(1, 100)
+
+while True:
+	num = input("猜一個數字: ")
+	num = int(num)
+	if num == r:
+		print("終於猜對了!")
+		break
+	elif num > r:
+		print("太大了!再猜")
+	else:
+		print("太小了!再猜")
